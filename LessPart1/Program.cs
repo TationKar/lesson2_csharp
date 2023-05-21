b905977a689438ef8881bccf1d9b6c3048e24bfd
Timur Karimov <tationkar@gmail.com>
2023-05-21 12:32:37 +0300

    part1_1

--- LessPart1/Program.cs
+++ LessPart1/Program.cs
@@ -8,16 +8,12 @@ int Max(int arg1, int arg2, int arg3)
     return result;
 }
 
-int a1=15, b1=21, c1=39, a2=12, b2=23, c2=33, a3=13, b3=23, c3=33;
+int a1=15, b1=21, c1=39, a2=128, b2=233, c2=33, a3=13, b3=23, c3=33;
 
-int max = a1;
-if(b1 >max) max = b1;
-if(c1 >max) max = c1;
-if(a2 >max) max = a2;
-if(b2 >max) max = b2;
-if(c2 >max) max = c2;
-if(a3 >max) max = a3;
-if(b3 >max) max = b3;
-if(c3 >max) max = c3;
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
 
-Console.WriteLine($"Max = {max}");
+int maxFin = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+Console.WriteLine($"Max = {maxFin}");
